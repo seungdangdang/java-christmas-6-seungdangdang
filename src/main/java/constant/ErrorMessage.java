b4d118b0a06f298c3d9a97950161ecb08a38d500
yunseung <yseungun.21@gmail.com>
2023-11-11 00:23:32 +0900

    feat(ErrorMessage): 에러메시지 생성

---
+++ src/main/java/constant/ErrorMessage.java
@@ -0,0 +1,10 @@
+package constant;
+
+public final class ErrorMessage {
+    private ErrorMessage() {
+    }
+
+    public static final String SYMBOL = "[ERROR] ";
+    public static final String NOT_VALID_NUMBER = "유효하지 않은 날짜입니다. 다시 입력해 주세요.";
+    public static final String NOT_VALID_ORDER = "유효하지 않은 주문입니다. 다시 입력해 주세요.";
+}
