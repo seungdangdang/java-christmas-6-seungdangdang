54e937fa5182ef9dbf5987d0c2c334d064250685
yunseung <yseungun.21@gmail.com>
2023-11-10 23:20:36 +0900

    feat(OutputView): 출력 메시지 추가
    - InputView 에 있던 출력 메시지 내용을 가져옴

--- src/main/java/io/OutputView.java
+++ src/main/java/io/OutputView.java
@@ -1,5 +1,6 @@
 package io;
 
+import static constant.RequestMessage.ORDER_REQUEST_MESSAGE;
 import static constant.RequestMessage.VISIT_DAY_REQUEST_MESSAGE;
 import static constant.SystemMessage.EVENT_PREVIEW_NOTIFICATION_MESSAGE;
 import static constant.SystemMessage.WELCOME_MESSAGE;
@@ -11,5 +12,13 @@ public class OutputView {
 
     public void eventPreviewMessage() {
         System.out.println(EVENT_PREVIEW_NOTIFICATION_MESSAGE);
+    }
+
+    public void visitDayRequestMessage() {
+        System.out.println(VISIT_DAY_REQUEST_MESSAGE);
+    }
+
+    public void orderRequestMessage() {
+        System.out.println(ORDER_REQUEST_MESSAGE);
     }
 }
