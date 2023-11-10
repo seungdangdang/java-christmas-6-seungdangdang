9e5b582ecd5f9c16e81a679ec8ca9b4eb02a1d6c
yunseung <yseungun.21@gmail.com>
2023-11-10 23:19:55 +0900

    remove(InputView): 출력 내용 삭제
    - 책임 분리를 위해 메시지 출력 내용은 OutputView 로 옮겼음

--- src/main/java/io/InputView.java
+++ src/main/java/io/InputView.java
@@ -1,27 +1,22 @@
 package io;
 
-import static constant.RequestMessage.MENU_REQUEST_MESSAGE;
-import static constant.RequestMessage.VISIT_DAY_REQUEST_MESSAGE;
-
 import camp.nextstep.edu.missionutils.Console;
 import java.util.HashMap;
 import java.util.Map;
 
 public class InputView {
-    public int getVisitDate() {
-        System.out.println(MENU_REQUEST_MESSAGE);
+    public int visitDate() {
         String input = Console.readLine();
         return Integer.parseInt(input);
     }
 
-    public Map<String, Integer> getMenuList() {
-        System.out.println(VISIT_DAY_REQUEST_MESSAGE);
+    public Map<String, Integer> orderList() {
         String input = Console.readLine();
         return parseOrder(input);
     }
 
     private static Map<String, Integer> parseOrder(String orderInfo) {
-        Map<String, Integer> orderMenuList = new HashMap<>();
+        Map<String, Integer> orderList = new HashMap<>();
 
         String[] items = orderInfo.split(",");
         for (String item : items) {
@@ -29,9 +24,9 @@ public class InputView {
             if (parts.length == 2) {
                 String menu = parts[0].trim();
                 int quantity = Integer.parseInt(parts[1].trim());
-                orderMenuList.put(menu, quantity);
+                orderList.put(menu, quantity);
             }
         }
-        return orderMenuList;
+        return orderList;
     }
 }
