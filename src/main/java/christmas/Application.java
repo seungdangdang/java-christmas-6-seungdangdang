6e7eee86aacfc0f3a154afa8c88244ce21e56bb2
yunseung <yseungun.21@gmail.com>
2023-11-10 23:24:59 +0900

    feat(Application): 게임 진행 클래스 연결

--- src/main/java/christmas/Application.java
+++ src/main/java/christmas/Application.java
@@ -1,7 +1,10 @@
 package christmas;
 
+import christmas.progress.Game;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        Game game = new Game();
+        game.run();
     }
 }
