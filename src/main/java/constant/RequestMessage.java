1e309deffd439351878520bcdced2d272e89cf71
yunseung <yseungun.21@gmail.com>
2023-11-10 23:21:24 +0900

    rename(RequestMessage): 상수명 변경

--- src/main/java/constant/RequestMessage.java
+++ src/main/java/constant/RequestMessage.java
@@ -2,7 +2,7 @@ package constant;
 
 public final class RequestMessage {
     public static final String VISIT_DAY_REQUEST_MESSAGE = "12월 중 식당 예상 방문 날짜는 언제인가요? (숫자만 입력해 주세요!)";
-    public static final String MENU_REQUEST_MESSAGE = "주문하실 메뉴를 메뉴와 개수를 알려 주세요. (e.g. 해산물파스타-2,레드와인-1,초코케이크-1)";
+    public static final String ORDER_REQUEST_MESSAGE = "주문하실 메뉴를 메뉴와 개수를 알려 주세요. (e.g. 해산물파스타-2,레드와인-1,초코케이크-1)";
 
     private RequestMessage() {
     }
