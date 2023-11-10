7434da156eaa2bd9fdb23606c7ca1857b0e31cc5
yunseung <yseungun.21@gmail.com>
2023-11-11 00:06:14 +0900

    feat(Game): 입력값을 저장하는 변수 선언

--- src/main/java/christmas/progress/Game.java
+++ src/main/java/christmas/progress/Game.java
@@ -2,17 +2,21 @@ package christmas.progress;
 
 import io.InputView;
 import io.OutputView;
+import java.util.HashMap;
+import java.util.Map;
 
 public class Game {
     public OutputView outputView = new OutputView();
     public InputView inputView = new InputView();
+    public int visitDate = 0;
+    public Map<String, Integer> orderList = new HashMap<>();
 
     public void run() {
         outputView.startMessage();
         outputView.visitDayRequestMessage();
-        inputView.visitDate();
+        this.visitDate = inputView.visitDate();
         outputView.orderRequestMessage();
-        inputView.orderList();
+        this.orderList = inputView.orderList();
         outputView.eventPreviewMessage();
     }
 }
