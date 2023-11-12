f1a151965ad989b272e1d287478ac3d26405c15c
yunseung <yseungun.21@gmail.com>
2023-11-12 17:23:38 +0900

    feat(MENU): enum 변수명 수정, 매개변수 추가
    - 메뉴이름을 변수명에서 매개변수로 변경

--- src/main/java/christmas/progress/MENU.java
+++ src/main/java/christmas/progress/MENU.java
@@ -1,25 +1,31 @@
 package christmas.progress;
 
 public enum MENU {
-    양송이수프(6000, "에피타이저"),
-    타파스(5500, "에피타이저"),
-    시저샐러드(8000, "에피타이저"),
-    티본스테이크(55000, "메인"),
-    바비큐립(54000, "메인"),
-    해산물파스타(35000, "메인"),
-    크리스마스파스타(25000, "메인"),
-    초코케이크(15000, "디저트"),
-    아이스크림(5000, "디저트"),
-    제로콜라(3000, "음료"),
-    레드와인(60000, "레드와인"),
-    샴페인(25000, "음료");
+    MUSHROOM_SOUP("양송이수프", 6000, "에피타이저"),
+    TAPAS("타파스", 5500, "에피타이저"),
+    CAESAR_SALAD("시저샐러드", 8000, "에피타이저"),
+    T_BONE_STEAK("티본스테이크", 55000, "메인"),
+    BBQ_RIB("바비큐립", 54000, "메인"),
+    SEAFOOD_PASTA("해산물파스타", 35000, "메인"),
+    CHRISTMAS_PASTA("크리스마스파스타", 25000, "메인"),
+    CHOCOLATE_CAKE("초코케이크", 15000, "디저트"),
+    ICE_CREAM("아이스크림", 5000, "디저트"),
+    ZERO_COLA("제로콜라", 3000, "음료"),
+    RED_WINE("레드와인", 60000, "와인"),
+    CHAMPAGNE("샴페인", 25000, "음료");
 
+    private final String menu;
     private final int price;
     private final String kind;
 
-    MENU(int price, String kind) {
+    MENU(String menu, int price, String kind) {
+        this.menu = menu;
         this.price = price;
         this.kind = kind;
+    }
+
+    public String getMenuName() {
+        return menu;
     }
 
     public int getMatchPrice() {
