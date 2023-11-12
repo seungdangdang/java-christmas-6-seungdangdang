5da0fb5fed65a7100ee7ce793df7644325601ea5
yunseung <yseungun.21@gmail.com>
2023-11-12 18:23:45 +0900

    feat(Validator): 입력 메뉴의 유효성검증 메서드 추가

--- src/main/java/validation/Validator.java
+++ src/main/java/validation/Validator.java
@@ -1,11 +1,26 @@
 package validation;
 
+import christmas.progress.MENU;
 import java.util.regex.Pattern;
 
 public class Validator {
+
     public void isDayOfMonthValid(String input) {
         final Pattern VISIT_DAY_PATTERN = Pattern.compile("^(?:[1-9]|[12]\\d|31)$");
         if (!VISIT_DAY_PATTERN.matcher(input).matches()) {
+            throw new IllegalArgumentException();
+        }
+    }
+
+    public void isValidMenu(String input) {
+        boolean isValid = false;
+        for (MENU menu : MENU.values()) {
+            if (menu.getKoreanName().equals(input)) {
+                isValid = true;
+                break;
+            }
+        }
+        if (!isValid) {
             throw new IllegalArgumentException();
         }
     }
