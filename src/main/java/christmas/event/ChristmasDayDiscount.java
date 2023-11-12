5a8bd63bb5fc07ad73d4070888ccca12744ff328
yunseung <yseungun.21@gmail.com>
2023-11-13 01:54:44 +0900

    refactor(ChristmasDayDiscount): 크리스마스디데이할인 계산 책임 분리

--- src/main/java/christmas/event/ChristmasDayDiscount.java
+++ src/main/java/christmas/event/ChristmasDayDiscount.java
@@ -1,26 +1,11 @@
 package christmas.event;
 
-import java.time.LocalDate;
-import java.time.temporal.ChronoUnit;
+import base.Calculator;
 
 public class ChristmasDayDiscount {
-    public int getDiscountChristmasDay(int visitDay) {
-        return calculateChristmasDayDiscount(visitDay);
-    }
+    Calculator calculator = new Calculator();
 
-    private static int calculateChristmasDayDiscount(int visitDay) {
-        LocalDate startDate = LocalDate.of(2023, 12, 1);
-        LocalDate endDate = LocalDate.of(2023, 12, 25);
-        LocalDate userDate = LocalDate.of(2023, 12, visitDay);
-
-        int initialAmount = 1000;
-        int discountPerDay = 100;
-
-        if (userDate.isAfter(startDate.minusDays(1)) && userDate.isBefore(endDate.plusDays(1))) {
-            int daysPassed = (int) startDate.until(userDate, ChronoUnit.DAYS);
-
-            return initialAmount + (daysPassed * discountPerDay);
-        }
-        return 0;
+    public int getChristmasDayDiscount(int visitDay) {
+        return calculator.calculateChristmasDayDiscount(visitDay);
     }
 }
