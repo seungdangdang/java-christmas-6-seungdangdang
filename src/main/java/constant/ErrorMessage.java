8a9a313faae090131644b3eba5eea387b2b3eaba
yunseung <yseungun.21@gmail.com>
2023-11-14 00:39:44 +0900

    chore(ErrorMessage): 에러메시지 앞 심볼 추가

--- src/main/java/constant/ErrorMessage.java
+++ src/main/java/constant/ErrorMessage.java
@@ -5,6 +5,6 @@ public final class ErrorMessage {
     }
 
     public static final String SYMBOL = "[ERROR] ";
-    public static final String NOT_VALID_NUMBER = "유효하지 않은 날짜입니다. 다시 입력해 주세요.";
-    public static final String NOT_VALID_ORDER = "유효하지 않은 주문입니다. 다시 입력해 주세요.";
+    public static final String NOT_VALID_NUMBER = SYMBOL + "유효하지 않은 날짜입니다. 다시 입력해 주세요.";
+    public static final String NOT_VALID_ORDER = SYMBOL + "유효하지 않은 주문입니다. 다시 입력해 주세요.";
 }
