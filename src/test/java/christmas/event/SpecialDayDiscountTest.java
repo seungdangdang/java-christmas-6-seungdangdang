7860e67645f8da6c648bdf55c7562a11cd315d2f
yunseung <yseungun.21@gmail.com>
2023-11-13 21:49:55 +0900

    test(SpecialDayDiscountTest): 특별할인 금액을 얻는 클래스 테스트 작성

---
+++ src/test/java/christmas/event/SpecialDayDiscountTest.java
@@ -0,0 +1,28 @@
+package christmas.event;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import base.Calculator;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+class SpecialDayDiscountTest {
+    private SpecialDayDiscount specialDayDiscount;
+    private Calculator calculator;
+
+    @BeforeEach
+    void setup() {
+        calculator = new Calculator();
+        specialDayDiscount = new SpecialDayDiscount(calculator);
+    }
+
+    @DisplayName("방문날짜가 스페셜데이라면 할인금액 1000원을 리턴한다.")
+    @Test
+    void setSpecialDayDiscount() {
+        assertEquals(specialDayDiscount.getSpecialDayDiscount(3), 1000);
+        assertEquals(specialDayDiscount.getSpecialDayDiscount(4), 0);
+        assertEquals(specialDayDiscount.getSpecialDayDiscount(31), 1000);
+    }
+
+}
