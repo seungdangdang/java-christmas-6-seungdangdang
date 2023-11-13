617639719af571e4a1dff7550e48f58d0c80bc5d
yunseung <yseungun.21@gmail.com>
2023-11-14 00:36:44 +0900

    feat(Benefit): 사용자의 혜택을 모두 얻는 기능 추가

---
+++ src/main/java/christmas/progress/Benefit.java
@@ -0,0 +1,57 @@
+package christmas.progress;
+
+import base.Calculator;
+import christmas.event.ChristmasDayDiscount;
+import christmas.event.DayOfWeekDiscount;
+import christmas.event.FreeGift;
+import christmas.event.SpecialDayDiscount;
+import java.util.Map;
+
+public class Benefit {
+    ChristmasDayDiscount christmasDayDiscount = new ChristmasDayDiscount();
+    DayOfWeekDiscount dayOfWeekDiscount = new DayOfWeekDiscount();
+    Calculator calculator = new Calculator();
+
+    SpecialDayDiscount specialDayDiscount = new SpecialDayDiscount(calculator);
+    FreeGift freeGift = new FreeGift();
+    private int christmasDayAmount = 0;
+    private int dayOfWeekAmount = 0;
+    private int specialDayAmount = 0;
+    private int freeGiftResult;
+    private String thisDay;
+
+    public int getChristmasDayAmount() {
+        return christmasDayAmount;
+    }
+
+    public int getDayOfWeekAmount() {
+        return dayOfWeekAmount;
+    }
+
+    public int getSpecialDayAmount() {
+        return specialDayAmount;
+    }
+
+    public int getFreeGiftResult() {
+        return freeGiftResult;
+    }
+
+    public String getThisDay() {
+        return thisDay;
+    }
+
+    public void getUserBenefit(int visitDay, Map<String, Integer> orderList, int beforeAmount) {
+        christmasDayAmount = christmasDayDiscount.getChristmasDayDiscount(visitDay);
+        dayOfWeekAmount = dayOfWeekDiscount.getDayOfWeekDiscountAmount(visitDay, orderList);
+        specialDayAmount = specialDayDiscount.getSpecialDayDiscount(visitDay);
+        if (calculator.getDiscountMenu(visitDay).equals("메인")) {
+            thisDay = "주말";
+        }
+        if (calculator.getDiscountMenu(visitDay).equals("디저트")) {
+            thisDay = "평일";
+        }
+        if (freeGift.getAboutFreeGift(beforeAmount).equals("샴페인 1개")) {
+            freeGiftResult = 25000;
+        }
+    }
+}
