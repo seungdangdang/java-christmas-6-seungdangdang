7f3de8b2b0dca6a76a2a2ff4419cd9749af8fe14
yunseung <yseungun.21@gmail.com>
2023-11-14 22:58:36 +0900

    refactor(UserVisitDate): 입력받는 책임 분리

--- src/main/java/common/userValue/UserVisitDate.java
+++ src/main/java/common/userValue/UserVisitDate.java
@@ -2,25 +2,27 @@ package common.userValue;
 
 import static constant.ErrorMessage.NOT_VALID_NUMBER;
 
-import camp.nextstep.edu.missionutils.Console;
 import common.tool.Converter;
+import io.Input;
 import validation.Validator;
 
-public class UserVisitDay {
+public class UserVisitDate {
     private final Converter converter = new Converter();
-
     private final Validator validator = new Validator();
+    private final Input input = new Input();
 
-    public int getVisitDay() {
+    public int getVisitDate() {
         return visitDate();
     }
 
+    //TODO: visitDate 메서드 책임 상세 분리
+    //TODO: visitDate 메서드 출력 방법 변경
     private int visitDate() {
         while (true) {
-            String input = Console.readLine();
+            String value = input.getUserInput();
             try {
-                validator.isDayOfMonthValid(input);
-                return converter.stringToInteger(input);
+                validator.isDayOfMonthValid(value);
+                return converter.stringToInteger(value);
             } catch (IllegalArgumentException dayError) {
                 System.out.println(NOT_VALID_NUMBER);
             }
