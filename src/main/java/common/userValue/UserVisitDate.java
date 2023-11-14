285d68ec8be80a2d11edad7121284400b43293eb
yunseung <yseungun.21@gmail.com>
2023-11-14 23:22:44 +0900

    refactor(UserVisitDay): 출력 책임 분리

--- src/main/java/common/userValue/UserVisitDate.java
+++ src/main/java/common/userValue/UserVisitDate.java
@@ -1,22 +1,21 @@
 package common.userValue;
 
-import static constant.ErrorMessage.NOT_VALID_NUMBER;
-
 import common.tool.Converter;
 import io.Input;
+import io.Output;
 import validation.Validator;
 
 public class UserVisitDate {
     private final Converter converter = new Converter();
     private final Validator validator = new Validator();
     private final Input input = new Input();
+    private final Output output = new Output();
 
     public int getVisitDate() {
         return visitDate();
     }
 
-    //TODO: visitDate 메서드 책임 상세 분리
-    //TODO: visitDate 메서드 출력 방법 변경
+    //TODO: visitDate 메서드 책임 상세 분리 (validator을 StringToInteger메서드에서)
     private int visitDate() {
         while (true) {
             String value = input.getUserInput();
@@ -24,7 +23,7 @@ public class UserVisitDate {
                 validator.isDayOfMonthValid(value);
                 return converter.stringToInteger(value);
             } catch (IllegalArgumentException dayError) {
-                System.out.println(NOT_VALID_NUMBER);
+                output.outputOrderErrorMessage();
             }
         }
     }
