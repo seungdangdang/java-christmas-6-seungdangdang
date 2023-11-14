24f494732e8599c26c84d4a507d3193b18a8f372
yunseung <yseungun.21@gmail.com>
2023-11-14 23:22:26 +0900

    refactor(UserOrder): 출력 책임 분리

--- src/main/java/common/userValue/UserOrder.java
+++ src/main/java/common/userValue/UserOrder.java
@@ -1,13 +1,13 @@
 package common.userValue;
 
-import static constant.ErrorMessage.NOT_VALID_ORDER;
-
 import common.tool.Converter;
 import io.Input;
+import io.Output;
 import java.util.Map;
 
 public class UserOrder {
     private final Input input = new Input();
+    private final Output output = new Output();
     private final Converter converter = new Converter();
 
     public Map<String, Integer> getOrderList() {
@@ -15,14 +15,13 @@ public class UserOrder {
     }
 
     //TODO: orderLlist 메서드 책임 상세 분리
-    //TODO: orderList 메서드 출력 방법 변경
     private Map<String, Integer> orderList() {
         while (true) {
             String value = input.getUserInput();
             try {
                 return converter.parseOrder(value);
             } catch (IllegalArgumentException | ArrayIndexOutOfBoundsException orderError) {
-                System.out.println(NOT_VALID_ORDER);
+                output.outputOrderErrorMessage();
             }
         }
     }
