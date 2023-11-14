32f7d53a1768b494498cca24b6588ae325b50a8b
yunseung <yseungun.21@gmail.com>
2023-11-14 23:14:23 +0900

    refactor(Input): 가지고 있던 책임 분리
    - Input 클래스는 직접적인 입력만 관리하도록 분리

--- src/main/java/io/Input.java
+++ src/main/java/io/Input.java
@@ -1,35 +1,10 @@
 package io;
 
-import static constant.ErrorMessage.NOT_VALID_ORDER;
-
-import common.tool.Converter;
 import camp.nextstep.edu.missionutils.Console;
-import java.util.Map;
-import validation.Validator;
 
 public class Input {
-    private final Converter converter = new Converter();
-
-    private final Validator validator = new Validator();
 
     public String getUserInput() {
         return Console.readLine();
-    }
-
-    public Map<String, Integer> orderList() {
-        while (true) {
-            String input = Console.readLine();
-            try {
-                return parseOrder(input);
-            } catch (IllegalArgumentException | ArrayIndexOutOfBoundsException orderError) {
-                System.out.println(NOT_VALID_ORDER);
-            }
-        }
-    }
-
-    private Map<String, Integer> parseOrder(String orderInfo) {
-        Map<String, Integer> orderList;
-        orderList = converter.parseOrderStringToList(orderInfo);
-        return orderList;
     }
 }
