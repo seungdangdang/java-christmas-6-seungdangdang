06988589ad6b421d33320f9505f4c060a5ae6fd6
yunseung <yseungun.21@gmail.com>
2023-11-15 01:23:29 +0900

    remove(Progress): 인터페이스 관련 기능 제거
    - 인터페이스를 분리함으로서 run() 메서드의 역할 감소

--- src/main/java/christmas/progress/Progress.java
+++ src/main/java/christmas/progress/Progress.java
@@ -1,37 +1,17 @@
 package christmas.progress;
 
-import common.tool.Calculator;
-import io.InputView;
-import io.OutputView;
 import java.util.HashMap;
 import java.util.Map;
-import validation.Validator;
 
 public class Progress {
-    public Validator validator = new Validator();
-    public OutputView outputView = new OutputView();
-    public InputView inputView = new InputView(validator);
-    Calculator calculator = new Calculator();
     public int visitDate = 0;
     public Map<String, Integer> orderList = new HashMap<>();
+    public UserInterface userInterface = new UserInterface();
 
     public void run() {
-        outputView.startMessage();
-        outputView.visitDayRequestMessage();
-        this.visitDate = inputView.visitDate();
-        outputView.orderRequestMessage();
-        this.orderList = inputView.orderList();
-        outputView.eventPreviewMessage(visitDate);
-        outputView.outputOrderMenu(orderList);
-        int beforeAmount = calculator.getBeforeDiscountAmount(orderList);
-        outputView.outputBeforeDiscountAmount(orderList);
-        outputView.outputFreeGift();
-        outputView.outputBenefit(visitDate, orderList, beforeAmount);
-        int entireDiscountAmount = outputView.getEntireDiscountAmount(visitDate, orderList, beforeAmount);
-        outputView.outputEntireBenefit(entireDiscountAmount);
-        int discountedAMount = outputView.getAfterDiscountedAmount(beforeAmount, entireDiscountAmount);
-        outputView.outputDiscountedAmount(discountedAMount);
-        String eventBadge = outputView.getEventBadge(entireDiscountAmount);
-        outputView.outputEventBadge(eventBadge);
+        userInterface.gatheringVisitDateAndOrderList();
+        this.visitDate = userInterface.visitDate;
+        this.orderList = userInterface.orderList;
+        userInterface.showEventBenefits(visitDate, orderList);
     }
 }
