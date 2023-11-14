ae03be0a6737bbe662d4016e2877161d054b1481
yunseung <yseungun.21@gmail.com>
2023-11-14 23:25:22 +0900

    refactor(UserVisitDate): 검증하는 기능 분리

--- src/main/java/common/userValue/UserVisitDate.java
+++ src/main/java/common/userValue/UserVisitDate.java
@@ -15,12 +15,10 @@ public class UserVisitDate {
         return visitDate();
     }
 
-    //TODO: visitDate 메서드 책임 상세 분리 (validator을 StringToInteger메서드에서)
     private int visitDate() {
         while (true) {
             String value = input.getUserInput();
             try {
-                validator.isDayOfMonthValid(value);
                 return converter.stringToInteger(value);
             } catch (IllegalArgumentException dayError) {
                 output.outputVisitDayErrorMessage();
