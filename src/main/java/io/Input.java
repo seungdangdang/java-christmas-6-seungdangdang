b72fe32766779b9c6e16de062e6a53eb0ac97f4e
yunseung <yseungun.21@gmail.com>
2023-11-15 21:54:09 +0900

    feat(Input): Input 클래스에서 사용자의 입력을 모두 받도록 책임 추가

--- src/main/java/io/Input.java
+++ src/main/java/io/Input.java
@@ -1,10 +1,45 @@
 package io;
 
 import camp.nextstep.edu.missionutils.Console;
+import common.tool.Converter;
+import java.util.Map;
 
 public class Input {
+    private final Converter converter = new Converter();
+    private final Output output = new Output(converter);
 
-    public String getUserInput() {
+    public Map<String, Integer> getOrderList() {
+        return orderList();
+    }
+
+    public int getVisitDate() {
+        return visitDate();
+    }
+
+    private int visitDate() {
+        while (true) {
+            String value = getUserInput();
+            try {
+                return converter.stringToInteger(value);
+            } catch (IllegalArgumentException dayError) {
+                output.outputVisitDayErrorMessage();
+            }
+        }
+    }
+
+    private Map<String, Integer> orderList() {
+        while (true) {
+            String value = getUserInput();
+            try {
+                return converter.parseOrder(value);
+            } catch (IllegalArgumentException | ArrayIndexOutOfBoundsException orderError) {
+                output.outputOrderErrorMessage();
+            }
+        }
+    }
+
+    private String getUserInput() {
         return Console.readLine();
     }
+
 }
