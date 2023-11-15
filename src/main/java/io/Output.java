c0b58f3ef3aaf7fdc19fcab9cf5c6a5709ddb7fd
yunseung <yseungun.21@gmail.com>
2023-11-15 22:26:32 +0900

    feat(Output): 메뉴 주문 시 주의사항 문구 출력

--- src/main/java/io/Output.java
+++ src/main/java/io/Output.java
@@ -19,6 +19,13 @@ public class Output {
         this.converter = converter;
     }
 
+    public final void outputInstructionsWhenOrder() {
+        System.out.println("* 총주문 금액 10,000원 이상부터 이벤트가 적용됩니다.");
+        System.out.println("* 음료만 주문 시, 주문할 수 없습니다.");
+        System.out.println("* 메뉴는 한 번에 최대 20개까지만 주문할 수 있습니다.\n"
+                + "(e.g. 시저샐러드-1, 티본스테이크-1, 크리스마스파스타-1, 제로콜라-3, 아이스크림-1의 총개수는 7개)");
+    }
+
     public final void startMessage() {
         System.out.println(WELCOME_MESSAGE);
     }
